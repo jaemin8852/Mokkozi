dd551a15abb74e946685afa855d07ebb549babbf
Jubil <jaemin8852@naver.com>
2018-09-05 17:58:01 +0900

    password compare thread

--- app/src/main/java/mokkozi/com/mokkozi/MainActivity.java
+++ app/src/main/java/mokkozi/com/mokkozi/MainActivity.java
@@ -1,5 +1,6 @@
 package mokkozi.com.mokkozi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/mokkozi/com/mokkozi/RegisterActivity.java
+++ app/src/main/java/mokkozi/com/mokkozi/RegisterActivity.java
@@ -1,23 +1,72 @@
 package mokkozi.com.mokkozi;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    int flag = 0;
+    EditText id;
     EditText pwd1;
     EditText pwd2;
+    Button previous_btn;
+    Button next_btn;
+    Handler mHandler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+
+        id = findViewById(R.id.register_id_edt);
         pwd1 = findViewById(R.id.register_pass_edt);
         pwd2 = findViewById(R.id.confirm_pass_edt);
-        /*버튼 내에서 동작
-        if(pwd1.getText().toString().equals(pwd2.getText().toString())) {
+        next_btn = findViewById(R.id.register_next_btn);
+        previous_btn = findViewById(R.id.regiset_previous_btn);
 
-        }*/
+        id.requestFocus();
 
+        previous_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //To do : flag가 0이면 인텐트 삭제하고 돌아가기
+                //        flag가 1이면 전화번호 입력 받는 창으로 가기
+            }
+        });
+
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //To do : flag가 0이면 인증하는 창으로 가기
+                //        flag가 1이면 서버로 데이터 전송 후, 인텐트 삭제하고 로그인 화면으로 돌아가기
+            }
+        });
+
+        mHandler = new Handler();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //여기서 x
+               mHandler.post(new Runnable () {
+
+                    @Override
+                    public void run() {
+                        while(true) {
+                            if (pwd1.getText().toString().equals(pwd2.getText().toString())) {
+                              Log.d("AAAAAAA", "is equal!!!!!!!!");
+                            }
+                            else {
+                                Log.d("hello", "NOT now!!");
+                            }
+                        }
+                    }
+                });
+            }
+        });
+        t.start();
     }
 }
